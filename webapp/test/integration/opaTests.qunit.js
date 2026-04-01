5869570b99fc299073445bec502193fd88502552
Puran Rana <puransinghrana080@gmail.com>
2026-04-01 04:53:35 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["com/demo/p5sapui5/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/demo/p5sapui5/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
