562bb7463f9f445f7c2ddcb100919dcc285bcfe9
Puran Rana <puransinghrana080@gmail.com>
2026-04-02 10:59:13 +0000

    add ObjectHeader IconTabBar, validation

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,12 +9,37 @@ sap.ui.define([
         onPress(){
             this.getOwnerComponent().getRouter().navTo("RouteView2")
         },
-        onSubmit(){
+        onSubmit1(){
             var name = this.getView().byId("idIpName").getValue();
             var msg = "Welcome to "+name;
             this.getView().byId("idTxtWelcome").setText(msg);
             this.getView().byId("idBtnSubmit").setType("Accept")
             this.getView().byId("l1").setRequired(false)
+        },
+        onSubmit2(){
+          
+            var empId = this.getView().byId('idEmpId').getValue();
+            if(empId === ""){
+                
+                this.getView().byId('idEmpId').setValueState("Error");
+                this.getView().byId('idEmpId').setValueStateText("Employee Id is mandatory please fill it");
+            }
+            else{
+                this.getView().byId('idEmpId').setValueState("None");
+
+                // Data format validation
+                var regExp = /^[a-zA-Z]+$/
+                if(!empId.match(regExp)){
+                    this.getView().byId('idEmpId').setValueState("Error");
+                    this.getView().byId('idEmpId').setValueStateText("Employee ID must be only alphabet");
+
+                }
+                // data format validation
+                // if(empId.length !==10){
+                //     this.getView().byId('idEmpId').setValueState("Error");
+                // this.getView().byId('idEmpId').setValueStateText("Employee Id should atleast be 10 digits");  
+                // }
+            }
         }
     });
 });
