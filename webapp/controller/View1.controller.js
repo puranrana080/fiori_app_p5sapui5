976871839e9293559575039fa8c5b731a065b664
Puran Rana <puransinghrana080@gmail.com>
2026-04-08 17:47:46 +0000

    v28 group asc desc

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,38 +4,55 @@ sap.ui.define([
     "sap/ui/model/Filter",
     "sap/ui/model/Sorter",
     "sap/ui/export/Spreadsheet"
-], (Controller,formatter,Filter,Sorter,Spreadsheet) => {
+], (Controller, formatter, Filter, Sorter, Spreadsheet) => {
     "use strict";
 
     return Controller.extend("com.demo.p5sapui5.controller.View1", {
-        f:formatter,
+        f: formatter,
         onInit() {
             this.getOwnerComponent().readEmployees()
+            this.mGroupFunctions = {
+                Skill:function(oContext){
+                    var skill = oContext.getProperty("Skill");
+                    return {
+                        key:skill,
+                        text: skill
+                    }
+                },
+                 Designation:function(oContext){
+                    var desig = oContext.getProperty("Designation");
+                    return {
+                        key:desig,
+                        text: desig
+                    }
+                }
+            };
+
         },
-        onPress(){
+        onPress() {
             this.getOwnerComponent().getRouter().navTo("RouteView2")
         },
-        onSubmit1(){
+        onSubmit1() {
             var name = this.getView().byId("idIpName").getValue();
-            var msg = "Welcome to "+name;
+            var msg = "Welcome to " + name;
             this.getView().byId("idTxtWelcome").setText(msg);
             this.getView().byId("idBtnSubmit").setType("Accept")
             this.getView().byId("l1").setRequired(false)
         },
-        onSubmit2(){
-          
+        onSubmit2() {
+
             var empId = this.getView().byId('idEmpId').getValue();
-            if(empId === ""){
-                
+            if (empId === "") {
+
                 this.getView().byId('idEmpId').setValueState("Error");
                 this.getView().byId('idEmpId').setValueStateText("Employee Id is mandatory please fill it");
             }
-            else{
+            else {
                 this.getView().byId('idEmpId').setValueState("None");
 
                 // Data format validation
                 var regExp = /^[a-zA-Z]+$/
-                if(!empId.match(regExp)){
+                if (!empId.match(regExp)) {
                     this.getView().byId('idEmpId').setValueState("Error");
                     this.getView().byId('idEmpId').setValueStateText("Employee ID must be only alphabet");
 
@@ -47,96 +64,109 @@ sap.ui.define([
                 // }
             }
         },
-        multiValueForm(){
+        multiValueForm() {
             var selBoxVal = this.getView().byId('idSel').getSelectedKey();
             var cbValue = this.getView().byId('idCb').getSelectedKey();
             var mcbValue = this.getView().byId('idMCb').getSelectedKeys();
-           var selIndex = this.getView().byId('idRBtn').getSelectedIndex();
+            var selIndex = this.getView().byId('idRBtn').getSelectedIndex();
         },
-        onSelFromSelectBox(oEvent){
-             var selBoxVal = this.getView().byId('idSel').getSelectedKey();
-             //oEvent.getParameter('selectedItem').getKey();
+        onSelFromSelectBox(oEvent) {
+            var selBoxVal = this.getView().byId('idSel').getSelectedKey();
+            //oEvent.getParameter('selectedItem').getKey();
         },
-        onSelFromCb(){
-             var cbValue = this.getView().byId('idCb').getSelectedKey();
-             //oEvent.getParameter('selectedItem').getKey();
+        onSelFromCb() {
+            var cbValue = this.getView().byId('idCb').getSelectedKey();
+            //oEvent.getParameter('selectedItem').getKey();
         },
-        onSelFromMCB(){
-             var mcbValue = this.getView().byId('idMCb').getSelectedKeys();
-             //oEvent.getParameter('selectedItem').getKeys();
+        onSelFromMCB() {
+            var mcbValue = this.getView().byId('idMCb').getSelectedKeys();
+            //oEvent.getParameter('selectedItem').getKeys();
         },
-        onSelFromRBtn(){
-             var selIndex = this.getView().byId('idRBtn').getSelectedIndex();
+        onSelFromRBtn() {
+            var selIndex = this.getView().byId('idRBtn').getSelectedIndex();
         },
-        getSelEmpData(){
+        getSelEmpData() {
             // var empId= this.getView().byId('idTable').getSelectedItem().getBindingContext().getProperty('Empid')
             var selBindingContext = this.getView().byId('idTable').getSelectedContext();
-            for(var i =0;i<selBindingContext.length;i++){
+            for (var i = 0; i < selBindingContext.length; i++) {
                 selBindingContext[i].getObject()
             }
         },
-        onPressRow(oEvent){
-          var empid = oEvent.getSource().getBindingContext().getProperty("Empid")
+        onPressRow(oEvent) {
+            var empid = oEvent.getSource().getBindingContext().getProperty("Empid")
 
         },
-        onPressRowFromF4HelpTable(oEvent){
-        var empid = oEvent.getSource().getBindingContext().getProperty("Empid");
-        this.getView().byId('idEmpID').setValue(empid);
-        this.oDialog.close();
+        onPressRowFromF4HelpTable(oEvent) {
+            var empid = oEvent.getSource().getBindingContext().getProperty("Empid");
+            this.getView().byId('idEmpID').setValue(empid);
+            this.oDialog.close();
         },
-        onPressValueHelp(){
+        onPressValueHelp() {
             //load the fragment
-            if(!this.oDialog){
-                  this.oDialog = sap.ui.xmlfragment(this.getView().getId(),"com.demo.p5sapui5.view.EmpidF4Help",this);
-                  this.getView().addDependent(this.oDialog);
+            if (!this.oDialog) {
+                this.oDialog = sap.ui.xmlfragment(this.getView().getId(), "com.demo.p5sapui5.view.EmpidF4Help", this);
+                this.getView().addDependent(this.oDialog);
             }
-            
+
             this.oDialog.open()
         },
-        onCloseDialog(){
+        onCloseDialog() {
             this.oDialog.close()
         },
-        onPressGo(){
-            var aFilters =[];
-            var aSorters=[];
+        onPressGo() {
+            var aFilters = [];
+            var aSorters = [];
             var empId = this.getView().byId('idEmpID').getValue();
-             var name = this.getView().byId('idName').getValue();
-              var desig = this.getView().byId('idDesig').getSelectedKey();
-               var skill = this.getView().byId('idSkill').getSelectedKey();
-               var salOpr = this.getView().byId('idSalOpr').getSelectedKey();
-               var salary = this.getView().byId('idSalary').getValue();
+            var name = this.getView().byId('idName').getValue();
+            var desig = this.getView().byId('idDesig').getSelectedKey();
+            var skill = this.getView().byId('idSkill').getSelectedKey();
+            var salOpr = this.getView().byId('idSalOpr').getSelectedKey();
+            var salary = this.getView().byId('idSalary').getValue();
             //    var doj = this.getView().byId('idDoj').getDateValue();
             //    doj = formatter.formatDateFilter(doj)
-            if(empId!==""){
-               aFilters.push(new Filter("Empid","EQ",empId));
+            if (empId !== "") {
+                aFilters.push(new Filter("Empid", "EQ", empId));
             }
-            if(name!==""){
-               aFilters.push(new Filter("Name","EQ",name));
+            if (name !== "") {
+                aFilters.push(new Filter("Name", "EQ", name));
             }
-            if(desig!==""){
-               aFilters.push(new Filter("Designation","EQ",desig));
+            if (desig !== "") {
+                aFilters.push(new Filter("Designation", "EQ", desig));
             }
-            if(skill!==""){
-               aFilters.push(new Filter("Skill","EQ",skill));
+            if (skill !== "") {
+                aFilters.push(new Filter("Skill", "EQ", skill));
             }
-            if(salary!==""){
-                aFilters.push(new Filter("Salary",salOpr,salary))
+            if (salary !== "") {
+                aFilters.push(new Filter("Salary", salOpr, salary))
             }
             // if(doj!==""){
             //     aFilters.push(new Filter("Doj",'EQ',doj) )
             // }
             this.getView().byId("idTable").getBinding('items').filter(aFilters);
+            // Grouping goes first
 
-            // Sorting
+             var groupField = this.getView().byId('idGroupField').getSelectedKey();
+            var groupOrder = this.getView().byId('idGroupOrder').getSelectedIndex();
+
+            if (groupField !== "" && groupOrder !== "") {
+                var vGroup = this.mGroupFunctions[groupField]
+                aSorters.push(new Sorter(groupField, (groupOrder === 0) ? false : true,vGroup))
+            }
+
+
+
+
+
+            // Sorting logic goes here
             var sortField = this.getView().byId('idSortField').getSelectedKey();
             var sortOrder = this.getView().byId('idSortOrder').getSelectedIndex();
-           
-            if(sortField!=="" && sortOrder!== ""){
-                aSorters.push(new Sorter(sortField,(sortOrder === 0)? false:true))
-            }   
+
+            if (sortField !== "" && sortOrder !== "") {
+                aSorters.push(new Sorter(sortField, (sortOrder === 0) ? false : true))
+            }
             this.getView().byId("idTable").getBinding('items').sort(aSorters);
         },
-        onPressReset(){
+        onPressReset() {
             this.getView().byId('idEmpID').setValue("");
             this.getView().byId('idName').setValue("");
             this.getView().byId('idDesig').setSelectedKey("");
@@ -149,57 +179,57 @@ sap.ui.define([
             this.getView().byId("idTable").getBinding('items').filter([]);
             this.getView().byId("idTable").getBinding('items').sort([]);
         },
-        onPressExportToXL(){
-            var aCols,oRowBinding,oSettings,oSheet;
+        onPressExportToXL() {
+            var aCols, oRowBinding, oSettings, oSheet;
             oRowBinding = this.getView().byId('idTable').getBinding('items')
             //place your table columns and odata properties
             aCols = [{
-                label:'Employee Id',
+                label: 'Employee Id',
                 property: 'Empid'
-            },{
-                label:'Name',
+            }, {
+                label: 'Name',
                 property: 'Name'
-            },{
-                label:'Designation',
+            }, {
+                label: 'Designation',
                 property: 'Designation'
-            },{
-                label:'Skill',
+            }, {
+                label: 'Skill',
                 property: 'Skill'
-            },{
-                label:'Email',
+            }, {
+                label: 'Email',
                 property: 'Email'
-            },{
-                label:'Phone.No',
+            }, {
+                label: 'Phone.No',
                 property: 'Phone'
-            },{
-                label:'Status',
+            }, {
+                label: 'Status',
                 property: 'Status'
-            },{
-                label:'Rating',
+            }, {
+                label: 'Rating',
                 property: 'Rating'
-            },{
-                label:'Date of Joining',
+            }, {
+                label: 'Date of Joining',
                 property: 'Doj',
-                type:'Date',
-                format:'dd-MM-yyyy'
-            },{
-                label:'Salary',
+                type: 'Date',
+                format: 'dd-MM-yyyy'
+            }, {
+                label: 'Salary',
                 property: 'Salary',
-                type:'Number',
-                delimiter:true,
-                scale:2
+                type: 'Number',
+                delimiter: true,
+                scale: 2
             }];
 
-            oSettings={
-                workbook:{
-                    columns:aCols
+            oSettings = {
+                workbook: {
+                    columns: aCols
                 },
                 dataSource: oRowBinding,
-                fileName:'Employees.xlsx',
-                worker:true
+                fileName: 'Employees.xlsx',
+                worker: true
             };
             oSheet = new Spreadsheet(oSettings);
-            oSheet.build().finally(function(){
+            oSheet.build().finally(function () {
                 oSheet.destroy();
             })
         }
